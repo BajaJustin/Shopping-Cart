5ada6423698968bf12637fb1866eedaa325489e3
BajaJustin <justindabaja@gmail.com>
2020-11-02 09:41:47 -0500

    Added function to cart items (inc,dec,amount)

---
+++ shopping-cart/src/data.js
@@ -0,0 +1,29 @@
+export default [{
+  id: 1,
+  item: 'PlayStation 5 Console',
+  price: 629.99,
+  img: 'https://i1.wp.com/freepngimages.com/wp-content/uploads/2020/07/Playstation-5-games-console-transparent-background-png-image.png?fit=1000%2C1000',
+  amount: 1
+},
+{
+  id: 2,
+  item: 'Playstation 5 Digital Edition',
+  price: 499.99,
+  img: 'https://gmedia.playstation.com/is/image/SIEPDC/playstation-5-digital-edition-with-dualsense-front-product-shot-01-ps5-en-30jul20?$native--t$',
+  amount: 1
+},
+{
+  id: 3,
+  item: 'Xbox Series X',
+  price: 599.99,
+  img: 'https://compass-ssl.xbox.com/assets/f0/85/f085c120-d3d5-4424-8b70-eb25deaa326e.png?n=XBX_A-BuyBoxBGImage01-D.png',
+  amount: 1
+},
+{
+  id: 4,
+  item: 'Xbox Series S',
+  price: 379.99,
+  img: 'https://compass-ssl.xbox.com/assets/c9/82/c9827ca6-83ee-457e-b33b-1c1a350efb57.png?n=0202999-BuyBox01_Image-D.png',
+  amount: 1
+}
+];
